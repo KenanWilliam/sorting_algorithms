9117abd7ef3a5181646495dac4c6989ed9a90c2a
KenanAllan <kenanallanwilliam@gmail.com>
2024-02-25 20:53:45 +0300

    c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,60 +1,26 @@
 #include "sort.h"
 
 /**
- * swap - swaps 2 ints
- * @a: the first integer
- * @b: the second integer
- * Return: void
-*/
-void swap(int *a, int *b)
-{
-	int temp;
-
-	temp = *a;
-	*a = *b;
-	*b = temp;
-}
-
-/**
- * get_gap - gets the gap using Knuth sequence
- * @size: the size of the array
- * Return: the gap
-*/
-size_t get_gap(size_t size)
-{
-	size_t gap = 1;
-
-	while (gap < size)
-	{
-		gap = 3 * gap + 1;
-	}
-	gap = (gap - 1) / 3;
-	return (gap);
-}
-/**
- * shell_sort - sorts the array using the shell sort
- * @array: the array to sort
- * @size: the size of the array
- * Return: void
-*/
+ * shell_sort - Shell sort - Knuth Sequence
+ * @array: array to sort
+ * @size: size of array
+ */
 void shell_sort(int *array, size_t size)
 {
-	size_t gap, j, i;
+	int gap = 1, i, j, tmp;
 
-	gap = get_gap(size);
-	for (; gap >= 1; gap = (gap - 1) / 3)
+	while (gap < ((int)(size)))
+		gap = (3 * gap) + 1;
+	for (gap = (gap - 1) / 3; gap > 0; gap = (gap - 1) / 3)
 	{
-		for (j = gap; j < size; j++)
+		for (i = gap; i < (int)size; i += 1)
 		{
-			for (i = j - gap; ; i = i - gap)
-			{
-				if (array[i + gap] < array[i])
-					swap(&array[i], &array[i + gap]);
-				else
-					break;
-				if (gap > i)
-					break;
-			}
+			tmp = array[i];
+
+			for (j = i; j >= gap && array[j - gap] > tmp; j -= gap)
+				array[j] = array[j - gap];
+
+			array[j] = tmp;
 		}
 		print_array(array, size);
 	}
